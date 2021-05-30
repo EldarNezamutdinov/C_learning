7c93b04a1d8deabead0cacc19bfaf9805703acca
EldarNezamutdinov <83502937+EldarNezamutdinov@users.noreply.github.com>
2021-05-30 16:27:35 +0700

    Studied variable pointers in the C programming language.

---
+++ Variable_addresses/addresses.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	system("chcp 1251>nul");
+
+	int num;
+	char sym;
+
+	int* p;
+	char* q;
+	p = &num;
+	q = &sym;
+
+	*p = 100;
+	*q = 'Z';
+	
+	
+	printf("Value of the variable num: \t%d\n", num);
+	printf("Value of the variable sym: \t%c\n", sym);
+
+	num = -100;
+	sym = 'A';
+
+	printf("Value at address num: \t%d\n", *p);
+	printf("Value at address sym: \t%c\n", *q);
+
+	system("pause>nul");
+	return 0;
+}
